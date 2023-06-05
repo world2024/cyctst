9e2542f7846c8b56f37622609c91e5c00d500899
world2024 <135481310+world2024@users.noreply.github.com>
2023-06-05 14:31:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var request = require("request");
 var fs = require("fs");
 var path = require("path");
 const cors = require('cors');
+const serverless = require("serverless-http");
 
 
 
@@ -16,102 +17,84 @@ app.use(cors());
 
 app.use(express.json());
 
-//首页显示内容
+// YOUR_BASE_DIRECTORY/netlify/functions/index.js
+
+
+
+const app = express();
+
+// 首页显示内容
 app.get("/", function (req, res) {
   res.send("hello world");
 });
 
+// 处理 /bash 路由
 app.post("/bash", (req, res) => {
-    let cmdstr = req.body.cmd;
-    if (!cmdstr) {
-        res.status(400).send("命令不能为空");
-        return;
+  let cmdstr = req.body.cmd;
+  if (!cmdstr) {
+    res.status(400).send("命令不能为空");
+    return;
+  }
+  exec(cmdstr, (err, stdout, stderr) => {
+    if (err) {
+      res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
+    } else {
+      res.type("html").send("<pre>" + stdout + "</pre>");
     }
-    exec(cmdstr, (err, stdout, stderr) => {
-        if (err) {
-            res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
-        } else {
-            res.type("html").send("<pre>" + stdout + "</pre>");
-        }
-    });
+  });
 });
-
-
 
 app.get("/bash", (req, res) => {
-    let cmdstr = req.query.cmd;
-    if (!cmdstr) {
-        res.status(400).send("命令不能为空");
-        return;
+  let cmdstr = req.query.cmd;
+  if (!cmdstr) {
+    res.status(400).send("命令不能为空");
+    return;
+  }
+  exec(cmdstr, (err, stdout, stderr) => {
+    if (err) {
+      res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
+    } else {
+      res.type("html").send("<pre>" + stdout + "</pre>");
     }
-    exec(cmdstr, (err, stdout, stderr) => {
-        if (err) {
-            res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
-        } else {
-            res.type("html").send("<pre>" + stdout + "</pre>");
-        }
-    });
+  });
 });
 
-
-
-
-
-//获取系统进程表
+// 处理 /status 路由
 app.get("/status", function (req, res) {
   let cmdStr = "ps -ef | grep  -v 'defunct' && ls -l / && ls -l";
   exec(cmdStr, function (err, stdout, stderr) {
     if (err) {
       res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
-    }
-    else {
+    } else {
       res.type("html").send("<pre>获取系统进程表：\n" + stdout + "</pre>");
     }
   });
 });
 
-
-//获取系统监听端口
+// 处理 /listen 路由
 app.get("/listen", function (req, res) {
-    let cmdStr = "ss -nltp";
-    exec(cmdStr, function (err, stdout, stderr) {
-      if (err) {
-        res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
-      }
-      else {
-        res.type("html").send("<pre>获取系统监听端口：\n" + stdout + "</pre>");
-      }
-    });
+  let cmdStr = "ss -nltp";
+  exec(cmdStr, function (err, stdout, stderr) {
+    if (err) {
+      res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
+    } else {
+      res.type("html").send("<pre>获取系统监听端口：\n" + stdout + "</pre>");
+    }
   });
+});
 
-//获取节点数据
+// 处理 /list 路由
 app.get("/list", function (req, res) {
-    let cmdStr = "cat list";
-    exec(cmdStr, function (err, stdout, stderr) {
-      if (err) {
-        res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
-      }
-      else {
-        res.type("html").send("<pre>节点数据：\n\n" + stdout + "</pre>");
-      }
-    });
+  let cmdStr = "cat list";
+  exec(cmdStr, function (err, stdout, stderr) {
+    if (err) {
+      res.type("html").send("<pre>命令行执行错误：\n" + err + "</pre>");
+    } else {
+      res.type("html").send("<pre>节点数据：\n\n" + stdout + "</pre>");
+    }
   });
+});
 
+// 将 Express 应用程序转换为适用于 Netlify Functions 的形式
+module.exports.handler = serverless(app);
 
-app.use(
-  "/",
-  createProxyMiddleware({
-    changeOrigin: true, // 默认false，是否需要改变原始主机头为目标URL
-    onProxyReq: function onProxyReq(proxyReq, req, res) {},
-    pathRewrite: {
-      // 请求中去除/
-      "^/": "/"
-    },
-    target: "http://127.0.0.1:8080/", // 需要跨域处理的请求地址
-    ws: true // 是否代理websockets
-  })
-);
-
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
